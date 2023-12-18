45d98a82ad6c1a10b80a10a4b81d1753618a4cde
Mariama Diallo <maridialloisidk@gmail.com>
2023-12-18 12:48:01 +0000

    Authentification des candidats avant leurs candidatures Authentification pour un administrateur de Simlpon Sénégal

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'matricule' => ('MaDiallo123'),
+            'nom' => ('Mariama'),
+            'prenom' => ('DIALLO'),
+            'telephone' => ('774958198'),
+            'email' => ('maridialloisidk@gmail.com'),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
